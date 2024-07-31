03c8666d3e1d61a88722af35768d484a58186ba1
pchai <prempreychhay@gmail.com>
2024-07-30 18:27:13 -0700

    Update 1:19:41

--- MVCCourse/WebApp/Models/CategoriesRepository.cs
+++ MVCCourse/WebApp/Models/CategoriesRepository.cs
@@ -7,7 +7,7 @@
 		{
 			new Category { CategoryId = 1, Name = "Beverage", Description = "Beverage"},
 			new Category { CategoryId = 2, Name = "Bakery", Description = "Bakery"},
-			new Category { CategoryId = 4, Name = "Meat", Description = "Meat"}
+			new Category { CategoryId = 3, Name = "Meat", Description = "Meat"}
 		};
 
 		// The create operation (C.R.U.D.)
